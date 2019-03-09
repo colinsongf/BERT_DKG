f4f5a1f5c068452dc7ea723736863af904bf9d6f
Stark_JC <401661801@qq.com>
2019-03-09 20:18:00 +0800

    init file

---
+++ Text_Clustering/flair_ner.py
@@ -0,0 +1,52 @@
+from flair.data import TaggedCorpus
+from flair.data_fetcher import NLPTaskDataFetcher, NLPTask
+from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings
+from typing import List
+from flair.training_utils import EvaluationMetric
+
+# define columns
+columns = {0: 'text', 1: 'pos', 2: 'chunk', 3: 'ner'}
+data_folder = r'D:\github\NER_projects\pt_bert_ner\data'
+corpus: TaggedCorpus = NLPTaskDataFetcher.load_column_corpus(data_folder, columns,
+                                                              train_file='train.txt',
+                                                              test_file='test.txt',
+                                                              dev_file='dev.txt')
+tag_type = 'ner'
+tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+
+embedding_types: List[TokenEmbeddings] = [
+
+    WordEmbeddings('glove'),
+
+    # comment in this line to use character embeddings
+    # CharacterEmbeddings(),
+
+    # comment in these lines to use flair embeddings
+    # FlairEmbeddings('news-forward'),
+    # FlairEmbeddings('news-backward'),
+]
+
+embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+
+from flair.models import SequenceTagger
+
+tagger: SequenceTagger = SequenceTagger(hidden_size=256,
+                                        embeddings=embeddings,
+                                        tag_dictionary=tag_dictionary,
+                                        tag_type=tag_type,
+                                        use_crf=True)
+
+from flair.trainers import ModelTrainer
+
+trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+
+trainer.train('resources/taggers/example-ner',
+              learning_rate=0.1,
+              mini_batch_size=32,
+              max_epochs=10)
+
+from flair.visual.training_curves import Plotter
+plotter = Plotter()
+plotter.plot_training_curves('resources/taggers/example-ner/loss.tsv')
+plotter.plot_weights('resources/taggers/example-ner/weights.txt')
---
+++ Text_Clustering/flair_text.py
@@ -0,0 +1,128 @@
+from __future__ import print_function
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn import metrics
+
+from sklearn.cluster import MiniBatchKMeans
+
+import logging
+from optparse import OptionParser
+import sys
+from time import time
+
+import numpy as np
+
+
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+# parse commandline arguments
+op = OptionParser()
+op.add_option("--lsa",
+              dest="n_components", type="int",
+              help="Preprocess documents with latent semantic analysis.")
+op.add_option("--no-minibatch",
+              action="store_false", dest="minibatch", default=True,
+              help="Use ordinary k-means algorithm (in batch mode).")
+op.add_option("--no-idf",
+              action="store_false", dest="use_idf", default=True,
+              help="Disable Inverse Document Frequency feature weighting.")
+op.add_option("--use-hashing",
+              action="store_true", default=True,
+              help="Use a hashing feature vectorizer")
+op.add_option("--n-features", type=int, default=10000,
+              help="Maximum number of features (dimensions)"
+                   " to extract from text.")
+op.add_option("--verbose",
+              action="store_true", dest="verbose", default=False,
+              help="Print progress reports inside k-means algorithm.")
+
+print(__doc__)
+op.print_help()
+
+
+def is_interactive():
+    return not hasattr(sys.modules['__main__'], '__file__')
+
+
+# work-around for Jupyter notebook and IPython console
+argv = [] if is_interactive() else sys.argv[1:]
+(opts, args) = op.parse_args(argv)
+if len(args) > 0:
+    op.error("this script takes no arguments.")
+    sys.exit(1)
+
+
+# #############################################################################
+# Load some categories from the training set
+categories = [
+    'alt.atheism',
+    'talk.religion.misc',
+    'comp.graphics',
+    'sci.space',
+]
+# Uncomment the following to do the analysis on all the categories
+# categories = None
+
+print("Loading 20 newsgroups dataset for categories:")
+print(categories)
+
+dataset = fetch_20newsgroups(subset='all', categories=categories,
+                             shuffle=True, random_state=42)
+
+print("%d documents" % len(dataset.data))
+print("%d categories" % len(dataset.target_names))
+print()
+
+labels = dataset.target
+true_k = np.unique(labels).shape[0]
+
+#############################################################################
+# get vectorized X
+from flair.embeddings import WordEmbeddings, DocumentPoolEmbeddings, FlairEmbeddings
+from flair.data import Sentence
+import sklearn.preprocessing as preprocessing
+glove_embedding = WordEmbeddings('glove')
+flair_embedding_forward = FlairEmbeddings('news-forward')
+flair_embedding_backward = FlairEmbeddings('news-backward')
+document_embeddings = DocumentPoolEmbeddings([glove_embedding,
+                                              flair_embedding_backward,
+                                              flair_embedding_forward])
+docs = [Sentence(doc) for doc in dataset.data]
+for doc in docs:
+    document_embeddings.embed(doc)
+
+X = [preprocessing.normalize(embed_doc.embedding.numpy(),axis=0) for embed_doc in docs]
+
+# #############################################################################
+# Do the actual clustering
+
+
+km = MiniBatchKMeans(n_clusters=true_k, init='k-means++', n_init=1,
+                         init_size=1000, batch_size=1000, verbose=opts.verbose)
+
+print("Clustering sparse data with %s" % km)
+t0 = time()
+km.fit(X)
+print("done in %0.3fs" % (time() - t0))
+print()
+
+print("--------------The larger the better---------------------")
+
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
+print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
+print("V-measure: %0.3f" % metrics.v_measure_score(labels, km.labels_))
+print("Adjusted Rand-Index: %.3f"
+      % metrics.adjusted_rand_score(labels, km.labels_))
+print("Normalized Mutual Information: %0.3f"
+      % metrics.normalized_mutual_info_score(labels, km.labels_))
+print("Rand index adjusted: %0.3f"
+      % metrics.adjusted_rand_score(labels, km.labels_))
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(X, km.labels_, sample_size=1000))
+print()
+
+print("--------------The lower the better---------------------")
+print("Davies-Bouldin score: %0.3f"
+      % metrics.davies_bouldin_score(X, km.labels_))
