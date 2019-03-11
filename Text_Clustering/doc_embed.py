8ee885e203d60ad6bc5a2ddf1b040d3a060ffee1
Stark_JC <401661801@qq.com>
2019-03-11 16:46:11 +0800

    init file

--- Text_Clustering/doc_embed.py
+++ Text_Clustering/doc_embed.py
@@ -67,7 +67,7 @@ def get_bert_embed(data):
         tokens_tensor = torch.tensor([doc_ids]).to(device)
         with torch.no_grad():
             encoded_layers, pooled_output = model(tokens_tensor, output_all_encoded_layers=False)
-        X.append(encoded_layers.sum(-1).tolist())
+        X.append(encoded_layers.sum(1).tolist())
     return X
 
 
