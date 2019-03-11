8b25431178119e653af39e78ddf38839c135c3d7
Stark_JC <401661801@qq.com>
2019-03-11 17:40:55 +0800

    init file

--- Text_Clustering/doc_embed.py
+++ Text_Clustering/doc_embed.py
@@ -8,7 +8,7 @@ def get_lda_embed(data):
     dictionary.filter_extremes(no_below=1, no_above=0.8)
     corpus = [dictionary.doc2bow(text) for text in docs]
 
-    lda = gensim.models.ldamodel.LdaModel(corpus=corpus, num_topics=50, alpha='auto', eval_every=5, update_every=5, chunksize=10000, passes=100)
+    lda = gensim.models.ldamodel.LdaModel(corpus=corpus, num_topics=50, alpha='auto', eval_every=5, update_every=5, chunksize=10000, passes=20)
     from gensim.test.utils import datapath
     import numpy as np
     temp_file = datapath("lda_model")
