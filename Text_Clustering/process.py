1a113d4046424d8a7c494b69edd7ef4e782e5248
Stark_JC <401661801@qq.com>
2019-03-12 11:59:22 +0800

    init file

--- Text_Clustering/process.py
+++ Text_Clustering/process.py
@@ -1,6 +1,9 @@
 import json
 import spacy
+import re
+
 nlp = spacy.load('en')
+r = re.compile("[\n]+")
 with open("data/ai_data_sents.txt", "w") as fw:
     with open("home/yjc/fc_out_academic.txt") as fr:
         len = 10000
@@ -9,7 +12,9 @@ with open("data/ai_data_sents.txt", "w") as fw:
             line = fr.readline()
             if not line:
                 break
-            doc = nlp(json.loads(line)['paperAbstract'])
+            doc = json.loads(line)['paperAbstract']
+            doc = re.sub(r, " ", doc)
+            doc = nlp(doc)
             fw.write('\n'.join([sent.text.strip() for sent in doc.sents])+'\n\n')
 
 
