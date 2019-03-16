591e972affb4b605d344757a540967f864594581
Stark_JC <401661801@qq.com>
2019-03-16 10:35:00 +0800

    init file

--- Text_Clustering/NER_projects/pt_bert_ner/dev_version/run_ner_regulization.py
+++ Text_Clustering/NER_projects/pt_bert_ner/dev_version/run_ner_regulization.py
@@ -18,8 +18,7 @@ from pytorch_pretrained_bert.tokenization import BertTokenizer
 from pytorch_pretrained_bert.modeling import BertPreTrainedModel, BertModel
 from pytorch_pretrained_bert.optimization import BertAdam
 
-from models import *
-from tools import valid_first
+from models import CRFDecoder, SoftmaxDecoder
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
@@ -29,6 +28,7 @@ logger = logging.getLogger(__name__)
 TRAIN = DEV = TEST = "tiny"
 
 TRAIN = "ai_data_train_labeled_140"
+UNLABELED_TRAIN = "ai_data_train_unlabeled_1400"
 DEV = "ai_data_dev46"
 TEST = "ai_data_test46"
 
@@ -37,19 +37,15 @@ class BertForNER(BertPreTrainedModel):
 
     def __init__(self, config, num_labels, decoder):
         super(BertForNER, self).__init__(config)
-        self.dropout_rate = config.hidden_dropout_prob
-        self.hidden_size = config.hidden_size
         self.bert = BertModel(config)
-        #self.bilstm = BiLSTM(self.hidden_size,self.hidden_size, 1, self.dropout_rate)
-        self.decoder = eval(decoder).create(num_labels, self.hidden_size, self.dropout_rate)
+        self.decoder = eval(decoder).create(num_labels, config.hidden_size, config.hidden_dropout_prob)
         self.apply(self.init_bert_weights)
 
     def forward(self, input_ids, segment_ids, input_mask, predict_mask, label_ids=None):
         ''' return mean loss of words or preds'''
-        hidden, _ = self.bert(input_ids, segment_ids, input_mask,
+        bert_layer, _ = self.bert(input_ids, segment_ids, input_mask,
                                   output_all_encoded_layers=False)  # bert_layer: (batch_size, max_seq_len, hidden_size)
-        #hidden = self.bilstm(hidden)
-        return self.decoder(hidden, predict_mask, label_ids)
+        return self.decoder(bert_layer, input_mask, label_ids)
 
 
 class InputExample(object):
@@ -124,11 +120,14 @@ class CONLLProcessor(DataProcessor):
 
     def get_test_examples(self, data_dir):
         return DataProcessor.create_examples_from_conll_format_file(os.path.join(data_dir, TEST + '.txt'), 'test')
-
+    
+    def get_unlabeled_train_examples(self, data_dir):
+        return DataProcessor.create_examples_from_conll_format_file(os.path.join(data_dir, UNLABELED_TRAIN+'.txt'), 'train_unlabeled')
+    
     @staticmethod
     def get_labels():
-        #return ['O', 'B-PER', 'I-PER', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC']
-        return ['O', 'B-FIELD', 'I-FIELD', 'B-TEC', 'I-TEC', 'B-MISC', 'I-MISC']
+        #return ['X', 'O', 'B-PER', 'I-PER', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC']
+        return ['X', 'O', 'B-FIELD', 'I-FIELD', 'B-TEC', 'I-TEC', 'B-MISC', 'I-MISC']
 
 
 def convert_examples_to_features(examples, max_seq_length, tokenizer, label_preprocessed, label_list):
@@ -206,6 +205,16 @@ def warmup_linear(x, warmup=0.002):
         return x / warmup
     return 1.0 - x
 
+def create_tensor_data(features):
+    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+    all_input_mask = torch.ByteTensor([f.input_mask for f in features])
+    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+    all_predict_mask = torch.ByteTensor([f.predict_mask for f in features])
+    all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
+    return TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_predict_mask, all_label_ids)
+
+
+
 def train():
     if config['train']['gradient_accumulation_steps'] < 1:
         raise ValueError("Invalid gradient_accumulation_steps parameter: {}, should be >= 1".format(
@@ -221,8 +230,15 @@ def train():
         torch.cuda.manual_seed_all(config['train']['seed'])
 
     train_examples = processor.get_train_examples(config['task']['data_dir'])
+    unlabeled_train_examples = processor.get_unlabeled_train_examples(config['task']['data_dir'])
+
+    # set steps
+    global_step = int(
+        (len(train_examples) + len(unlabeled_train_examples)) / config['train']['batch_size'] / config['train'][
+            'gradient_accumulation_steps'] * start_epoch)
     num_train_steps = int(
-        len(train_examples) / config['train']['batch_size'] / config['train']['gradient_accumulation_steps']) * \
+        (len(train_examples) + len(unlabeled_train_examples))/2 / config['train']['batch_size'] / config['train'][
+            'gradient_accumulation_steps']) * \
                       config['train']['epochs']
     # Prepare optimizer
     param_optimizer = list(model.named_parameters())
@@ -238,61 +254,66 @@ def train():
     train_features, train_tokenize_info = convert_examples_to_features(train_examples, max_seq_length,
                                                                        tokenizer,
                                                                        label_preprocessed, label_list)
-    with codecs.open(os.path.join(config['task']['output_dir'], "train.tokenize_info"), 'w',
-                     encoding='utf-8') as f:
-        for item in train_tokenize_info:
-            f.write(' '.join([str(num) for num in item]) + '\n')
+    unlabeled_train_features, unlabeled_train_tokenize_info = convert_examples_to_features(unlabeled_train_examples, max_seq_length,
+                                                                               tokenizer,
+                                                                               label_preprocessed, label_list)
 
-    logger.info("***** Running training *****")
-    logger.info("  Num examples = %d", len(train_examples))
-    logger.info("  Batch size = %d", config['train']['batch_size'])
-    logger.info("  Num steps = %d", num_train_steps)
-    all_input_ids = torch.tensor([f.input_ids for f in train_features], dtype=torch.long)
-    all_input_mask = torch.ByteTensor([f.input_mask for f in train_features])
-    #all_input_mask = torch.tensor([f.input_mask for f in train_features], dtype=torch.long)
-    all_segment_ids = torch.tensor([f.segment_ids for f in train_features], dtype=torch.long)
-    all_predict_mask = torch.ByteTensor([f.predict_mask for f in train_features])
-    all_label_ids = torch.tensor([f.label_ids for f in train_features], dtype=torch.long)
-    train_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_predict_mask, all_label_ids)
+    train_data = create_tensor_data(train_features)
     train_sampler = RandomSampler(train_data)
     train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=config['train']['batch_size'])
 
-    global_step = int(
-        len(train_examples) / config['train']['batch_size'] / config['train'][
-            'gradient_accumulation_steps'] * start_epoch)
+    unlabeled_train_data = create_tensor_data(unlabeled_train_features)
+    unlabeled_train_sampler = SequentialSampler(unlabeled_train_data)
+    unlabeled_data_loader = DataLoader(unlabeled_train_data, sampler=unlabeled_train_sampler, batch_size=config['train']['batch_size'])
+
+    logger.info("***** Running training*****")
+    weight = torch.tensor(1., requires_grad=False).to(device)
+    train_loss_list = []
+    dev_loss_list = []
     for epoch in trange(start_epoch, config['train']['epochs'], desc="Epoch"):
         model.train()
         tr_loss = 0
         nb_tr_examples, nb_tr_steps = 0, 0
+        unlabeled_iter = iter(unlabeled_data_loader)
+        weight.fill_(1.)
         for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
             batch = tuple(t.to(device) for t in batch)
             input_ids, input_mask, segment_ids, predict_mask, label_ids = batch
-
             loss = model(input_ids, segment_ids, input_mask, predict_mask, label_ids)
+
+            if epoch>2:
+                batch = unlabeled_iter.next()
+                batch = tuple(t.to(device) for t in batch)
+                input_ids, input_mask, segment_ids, predict_mask, label_ids = batch
+                _, probs = model(input_ids, segment_ids, input_mask, predict_mask)
+                unlabeled_loss = -((probs.log()*probs).sum(-1)*predict_mask.float()).mean()
+                loss = weight*unlabeled_loss + loss
+
+                #print("unlabeled loss: %.3f; \nlabeled loss: %.3f; " % (unlabeled_loss.item(), loss.item()))
 
             if config['n_gpu'] > 1:
                 loss = loss.mean()  # mean() to average on multi-gpu.
-
             if config['train']['gradient_accumulation_steps'] > 1:
                 loss = loss / config['train']['gradient_accumulation_steps']
-
             loss.backward()
             tr_loss += loss.item()
             if (step + 1) % config['train']['gradient_accumulation_steps'] == 0:
                 # modify learning rate with special warm up BERT uses
                 lr_this_step = config['train']['learning_rate'] * warmup_linear(global_step / num_train_steps,
-                                                                                config['train'][
-                                                                                    'warmup_proportion'])
+                                                                                config['train']['warmup_proportion'])
                 for param_group in optimizer.param_groups:
                     param_group['lr'] = lr_this_step
                 optimizer.step()
                 optimizer.zero_grad()
                 global_step += 1
+                weight += 1.
             nb_tr_examples += input_ids.size(0)
             nb_tr_steps += 1
-        logger.info("memory usage: %.4f" % memory_usage_psutil())
-        logger.info("epoch loss (mean word loss): %.4f" % (tr_loss / nb_tr_steps))
+
+        # logger.info("memory usage: %.4f" % memory_usage_psutil())
+        # logger.info("epoch loss (mean word loss): %.4f" % (tr_loss / nb_tr_steps))
         train_loss_list.append(tr_loss / nb_tr_steps)
+
         if config['dev']['do_every_epoch']:
             dev_loss_list.append(evaluate(config['dev']['dataset'], epoch))
 
@@ -302,14 +323,29 @@ def train():
         # Save a checkpoint
         model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
         torch.save({'epoch': epoch, 'model_state': model_to_save.state_dict(), 'max_seq_length': max_seq_length,
-                    'lower_case': lower_case, 'train_loss_list':train_loss_list, 'dev_loss_list':dev_loss_list},
+                    'lower_case': lower_case},
                    os.path.join(config['task']['output_dir'], 'checkpoint-%d' % epoch))
     if not config['dev']['do_every_epoch'] and config['dev']['do_after_train']:
         evaluate(config['dev']['dataset'])
     if not config['test']['do_every_epoch'] and config['test']['do_after_train']:
         evaluate(config['test']['dataset'])
 
-    draw(train_loss_list, dev_loss_list, config['train']['epochs'])
+    draw(train_loss_list, dev_loss_list, start_epoch, config['train']['epochs'])
+
+def draw(train, dev,start, end):
+    import matplotlib.pyplot as plt
+    plt.switch_backend('agg')
+    x = range(start+1, end+1)
+    plt.plot(x, train, color='green', marker='o')
+    legend = ['train']
+    if dev !=[]:
+        plt.plot(x, dev, color='red', marker='+')
+        legend.append('dev')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(legend)
+    plt.title("Loss on "+config['task']['decoder'])
+    plt.savefig(os.path.join(config['task']['output_dir'],config['task']['decoder']+"_loss.jpg"))
 
 def evaluate(dataset, train_steps=None):
     if dataset == 'train':
@@ -331,7 +367,6 @@ def evaluate(dataset, train_steps=None):
     logger.info("  Batch size = %d", config[dataset]['batch_size'])
     all_input_ids = torch.tensor([f.input_ids for f in eval_features], dtype=torch.long)
     all_input_mask = torch.ByteTensor([f.input_mask for f in eval_features])
-    #all_input_mask = torch.tensor([f.input_mask for f in eval_features], dtype=torch.long)
     all_segment_ids = torch.tensor([f.segment_ids for f in eval_features], dtype=torch.long)
     all_eval_mask = torch.ByteTensor([f.predict_mask for f in eval_features])
     all_label_ids = torch.tensor([f.label_ids for f in eval_features], dtype=torch.long)
@@ -342,7 +377,6 @@ def evaluate(dataset, train_steps=None):
                                  batch_size=config[dataset]['batch_size'])
     model.eval()
     predictions = []
-    predict_masks = []
     eval_loss, eval_accuracy = 0, 0
     nb_eval_steps, nb_eval_examples = 0, 0
     for batch in tqdm(eval_dataloader, desc="Evaluating"):
@@ -350,10 +384,9 @@ def evaluate(dataset, train_steps=None):
         input_ids, input_mask, segment_ids, predict_mask, label_ids = batch
         with torch.no_grad():
             tmp_eval_loss = model(input_ids, segment_ids, input_mask, predict_mask, label_ids)
-            outputs = model(input_ids, segment_ids, input_mask, predict_mask)
-        reshaped_predict_mask, reshaped_label_ids, _ = valid_first(predict_mask, label_ids)
+            outputs, _ = model(input_ids, segment_ids, input_mask, predict_mask)
         masked_label_ids = torch.masked_select(label_ids, predict_mask)
-        masked_outputs = torch.masked_select(outputs, reshaped_predict_mask)
+        masked_outputs = torch.masked_select(outputs, predict_mask)
         masked_label_ids = masked_label_ids.cpu().numpy()
         masked_outputs = masked_outputs.detach().cpu().numpy()
 
@@ -362,7 +395,7 @@ def evaluate(dataset, train_steps=None):
 
         tmp_eval_accuracy = accuracy(masked_outputs, masked_label_ids)
         predictions.extend(outputs.detach().cpu().numpy().tolist())
-        predict_masks.extend(reshaped_predict_mask.detach().cpu().numpy().tolist())
+
         if config['n_gpu'] > 1:
             tmp_eval_loss = tmp_eval_loss.mean()  # mean() to average on multi-gpu.
 
@@ -372,46 +405,24 @@ def evaluate(dataset, train_steps=None):
         nb_eval_steps += 1
     eval_loss = eval_loss / nb_eval_steps
     eval_accuracy = eval_accuracy / nb_eval_examples
-    logger.info('eval_loss: %.4f; eval_accuracy: %.4f' % (eval_loss,eval_accuracy))
+    logger.info('eval_loss: %.4f; eval_accuracy: %.4f' % (eval_loss, eval_accuracy))
     if train_steps is not None:
-        fn1 = "%s.predict_epoch_%s " % (dataset, train_steps)
-        fn2 = "%s.mistake_epoch_%s " % (dataset, train_steps)
+        fn = "%s.predict_epoch_%s " % (dataset, train_steps)
     else:
-        fn1 = "%s.predict " % dataset
-        fn2 = "%s.mistake " % dataset
-    writer1 = codecs.open(os.path.join(config['task']['output_dir'], fn1), 'w', encoding='utf-8')
-    writer2 = codecs.open(os.path.join(config['task']['output_dir'], fn2), 'w', encoding='utf-8')
-    for example, predict_line, predict_mask in zip(eval_examples, predictions, predict_masks):
-        w1_sent = []
+        fn = "%s.predict " % dataset
+    writer = codecs.open(os.path.join(config['task']['output_dir'], fn), 'w', encoding='utf-8')
+    for example, predict_line, feature in zip(eval_examples, predictions, eval_features):
+        sent = []
         word_idx = 0
-        mistake = False
-        for index, label_id in enumerate(predict_line[:sum(predict_mask)]):
-            line = ' '.join([example.words[word_idx], example.labels[word_idx], label_list[label_id]])
-            w1_sent.append(line)
-            if label_list[label_id] != example.labels[word_idx]:
-                mistake = True
-            word_idx += 1
-        writer1.write('\n'.join(w1_sent) + '\n\n')
-        if mistake: writer2.write('\n'.join(w1_sent) + '\n\n')
-    writer1.close()
-    writer2.close()
+        for index, label_id in enumerate(predict_line[:sum(feature.input_mask)]):
+            if feature.predict_mask[index] == 1:
+                sent.append(' '.join([example.words[word_idx], example.labels[word_idx], 'O' if label_id == 0 else label_list[label_id]]))
+                word_idx += 1
+            if feature.input_mask[index] == 0:
+                break
+        writer.write('\n'.join(sent) + '\n\n')
+    writer.close()
     return eval_loss
-
-def draw(train, dev,end):
-    import matplotlib.pyplot as plt
-    plt.switch_backend('agg')
-    x = range(1, end+1)
-    plt.plot(x, train, color='green', marker='o')
-    legend = ['train']
-    if dev !=[]:
-        plt.plot(x, dev, color='red', marker='+')
-        legend.append('dev')
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    plt.legend(legend)
-    plt.title("Loss on "+config['task']['decoder'])
-    plt.savefig(os.path.join(config['task']['output_dir'], config['task']['decoder'] + "_loss.jpg"))
-
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
@@ -456,22 +467,19 @@ if __name__ == "__main__":
             lower_case = checkpoint['lower_case']
             model = BertForNER.from_pretrained(config['task']['bert_model_dir'], state_dict=checkpoint['model_state'],
                                                num_labels=len(label_list), decoder=config['task']['decoder'])
-            train_loss_list = checkpoint['train_loss_list']
-            dev_loss_list = checkpoint['dev_loss_list']
         else:
             start_epoch = 0
             max_seq_length = config['task']['max_seq_length']
             lower_case = config['task']['lower_case']
             model = BertForNER.from_pretrained(config['task']['bert_model_dir'], num_labels=len(label_list),
                                                decoder=config['task']['decoder'])
-            train_loss_list = []
-            dev_loss_list = []
 
         tokenizer = BertTokenizer.from_pretrained(config['task']['bert_model_dir'], do_lower_case=lower_case)
 
         model.to(device)
         if config['n_gpu'] > 1:
             model = torch.nn.DataParallel(model)
+
         if config['train']['do']:
             train()
         if config['test']['do']:
