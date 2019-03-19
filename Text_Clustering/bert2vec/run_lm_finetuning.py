fe7c3ba73bac679a8adcaec3daf6a4168c3d9096
Stark_JC <401661801@qq.com>
2019-03-19 20:28:40 +0800

    init file

--- Text_Clustering/bert2vec/run_lm_finetuning.py
+++ Text_Clustering/bert2vec/run_lm_finetuning.py
@@ -272,7 +272,7 @@ def main(train_file, args):
     # Prepare model
     bert_config = BertConfig(args.bert_config)
     bert_config.type_vocab_size = len(train_dataset)
-    output_model_file = os.path.join(args.output_dir, "model.zip")
+    output_model_file = os.path.join(args.output_dir, "pytorch_model.bin.gz")
     if os.path.exists(output_model_file):
         model = BertForPreTraining.from_pretrained(output_model_file)
         args.do_train = False
