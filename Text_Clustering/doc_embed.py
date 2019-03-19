6bee8ceb11f72a257c6ebebb09a7cc47f6d518f7
Stark_JC <401661801@qq.com>
2019-03-19 11:21:12 +0800

    init file

--- Text_Clustering/doc_embed.py
+++ Text_Clustering/doc_embed.py
@@ -26,9 +26,9 @@ def get_doc2vec_embed(data):
     from gensim.models.doc2vec import Doc2Vec, TaggedDocument
     import sklearn.preprocessing as preprocessing
     docs = [TaggedDocument(gensim.utils.simple_preprocess(doc), [i]) for i, doc in enumerate(data)]
-    doc2vec_model = Doc2Vec(vector_size=300, min_count=1, max_count=1000)
+    doc2vec_model = Doc2Vec(dm=0, vector_size=300, min_count=2, max_count=1000, iter=10)
     doc2vec_model.build_vocab(docs)
-    doc2vec_model.train(docs, total_examples=doc2vec_model.corpus_count, epochs=10)
+    doc2vec_model.train(docs, total_examples=doc2vec_model.corpus_count, epoch=10)
     doc2vec_X = [doc2vec_model.infer_vector(doc.words) for doc in docs]
     X = preprocessing.normalize(doc2vec_X)
     return X
