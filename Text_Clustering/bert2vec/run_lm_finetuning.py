685ff72ef073a9b6b31b27445d16e99ca01350af
Stark_JC <401661801@qq.com>
2019-03-22 11:24:20 +0800

    init file

--- Text_Clustering/bert2vec/run_lm_finetuning.py
+++ Text_Clustering/bert2vec/run_lm_finetuning.py
@@ -155,8 +155,8 @@ def random_word(tokens, tokenizer):
     for i, token in enumerate(tokens):
         prob = random.random()
         # mask token with 15% probability
-        if prob < 1:
-            prob /= 1
+        if prob < 0.9:
+            prob /= 0.9
             tokens[i] = "[MASK]"
             masked = True
             # # 80% randomly change token to mask token
