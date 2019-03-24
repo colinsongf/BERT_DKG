0563580cc21234dab6994fce42876f7a62cc953f
Stark_JC <401661801@qq.com>
2019-03-24 17:27:52 +0800

    init file

--- Text_Clustering/process.py
+++ Text_Clustering/process.py
@@ -44,6 +44,35 @@ def json_to_conll():
                         print(i)
 
 
+def json_to_conll2():
+    nlp = spacy.load('en')
+    r1 = re.compile("[\n]+")
+    r2 = re.compile("[\W]{3,}")
+    defined_words = set(["Information retrieval", "Information sciences", "Information science"])
+    with open("data/ai_data_sents_new.txt", "w") as fw1:
+        with open("/home/yjc/fc_out_academic.txt") as fr:
+            max_len = 10000
+            i = 1
+            while i < max_len:
+                line = fr.readline()
+                if not line:
+                    break
+                ob = json.loads(line)
+                if set(ob["entities"]).intersection(defined_words) != set():
+                    doc = ob['paperAbstract']
+                    doc = re.sub(r1, " ", doc)
+                    doc = re.sub(r2, " ", doc)
+                    if len(doc.strip(" ")) > 20 and len(doc.strip(" ")) < 1000:
+                        doc = nlp(doc)
+                        fw1_ = []
+                        for sent in doc.sents:
+                            if len(sent.text.strip()) > 10:
+                                fw1_.append(sent.text.strip())
+                        fw1.write('\n'.join(fw1_) + "\n\n")
+                        i += 1
+            print(i)
+
+
 def extract_from_json():
     r1 = re.compile("[\n]+")
     r2 = re.compile("[\W]{3,}")
@@ -63,4 +92,4 @@ def extract_from_json():
             print(i)
 
 
-extract_from_json()
+json_to_conll2()
