99b70760c315dd7b6d2000a4c392d0cd08cada9f
Stark_JC <401661801@qq.com>
2019-04-05 15:15:40 +0800

    init file

--- Text_Clustering/bert2vec/run_lm_finetuning.py
+++ Text_Clustering/bert2vec/run_lm_finetuning.py
@@ -29,8 +29,8 @@ from torch.utils.data import DataLoader, Dataset, RandomSampler
 from torch.utils.data.distributed import DistributedSampler
 from tqdm import tqdm, trange
 import sklearn.preprocessing as preprocessing
-from modeling import MyBertForPreTraining, BertConfig
-from optimization import BertAdam, warmup_linear
+from .modeling import MyBertForPreTraining, BertConfig
+from .optimization import BertAdam, warmup_linear
 import gensim
 from torch.utils.data import Dataset
 import random
