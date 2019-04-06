70859bb9e29b912b837d7d882378ce208111ba55
Stark_JC <401661801@qq.com>
2019-04-06 10:05:10 +0800

    init file

--- Text_Clustering/run_text_cluster.py
+++ Text_Clustering/run_text_cluster.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.INFO,
 
 # parse commandline arguments
 op = OptionParser()
-op.add_option("--embed_type", dest="embed_type", default="get_tfidf_embed")
+op.add_option("--embed_type", dest="embed_type", default="get_bert2vec_embed")
 op.add_option("--run_num", dest="run_num", type=int, default=10)
 op.add_option("--cluster_num", dest="cluster_num", type=int, default=8)
 op.add_option("--doc_path", dest="doc_path", default="ai")
