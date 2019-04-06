fa3591b0cb1fa2ec4d433e506e2d4d7215effbd9
Stark_JC <401661801@qq.com>
2019-04-06 12:53:16 +0800

    init file

--- Text_Clustering/bert2vec/modeling.py
+++ Text_Clustering/bert2vec/modeling.py
@@ -271,9 +271,9 @@ class BertEmbeddings(nn.Module):
         position_embeddings = self.position_embeddings(position_ids)
         doc_embeddings = self.doc_embeddings(token_type_ids)
 
-        # embeddings = words_embeddings + doc_embeddings + position_embeddings
-        embeddings = (words_embeddings + doc_embeddings).sum(-2) + position_embeddings[:, 0]
-        embeddings = embeddings.unsqueeze(1).expand(batch_size, seq_length, self.hidden_size)
+        embeddings = words_embeddings + doc_embeddings + position_embeddings
+        # embeddings = (words_embeddings + doc_embeddings).sum(-2) + position_embeddings[:, 0]
+        # embeddings = embeddings.unsqueeze(1).expand(batch_size, seq_length, self.hidden_size)
         embeddings = self.LayerNorm(embeddings)
         embeddings = self.dropout(embeddings)
         return embeddings
