91c126c615b90de3d5dd1a5211dd21703fe0fb27
Stark_JC <401661801@qq.com>
2019-04-10 11:13:58 +0800

    init file

--- Text_Clustering/run_text_cluster.py
+++ Text_Clustering/run_text_cluster.py
@@ -270,17 +270,17 @@ def hook_doc(dataset, X):
             #
 
             # 选择top3 field以及相应的tec
-            # fields_ = dict(sorted(fields.items(), key=lambda x: x[1][1], reverse=True)[:field_top])
+            fields_ = dict(sorted(fields.items(), key=lambda x: x[1][1], reverse=True)[:field_top])
 
             # 选择1%之后的几个
             # fields_ = dict(sorted(fields.items(), key=lambda x: x[1][1], reverse=True)[
             #                int(len(fields) * 0.01):int(len(fields) * 0.05) + field_top])
 
 
-            filter_fields = dict([f for f in fields.items() if f[0] not in pre_fields])
-            #fields_ = dict(sorted(filter_fields.items(), key=lambda x: x[1][1], reverse=True)[int(len(filter_fields)*0.01):int(len(filter_fields)*0.05)+field_top])
-            fields_ = dict(sorted(filter_fields.items(), key=lambda x: x[1][1], reverse=True)[:field_top])
-            pre_fields.update(set([(f[0],f[1][2]) for f in fields_.items()]))
+            # filter_fields = dict([f for f in fields.items() if f[0] not in pre_fields])
+            # #fields_ = dict(sorted(filter_fields.items(), key=lambda x: x[1][1], reverse=True)[int(len(filter_fields)*0.01):int(len(filter_fields)*0.05)+field_top])
+            # fields_ = dict(sorted(filter_fields.items(), key=lambda x: x[1][1], reverse=True)[:field_top])
+            # pre_fields.update(set([(f[0],f[1][2]) for f in fields_.items()]))
 
             used_fields = [i[1][-1] for i in fields_.items()]
             co_occurence_ = [i for i in co_occurence.items() if i[0][0] in used_fields]
